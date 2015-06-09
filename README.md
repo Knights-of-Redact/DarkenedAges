4ed0809d64411d3be6e11f40b2e9fd738a86124f
Non Boong <nonboong@localhost.localdomain>
2015-06-10 03:20:58 +0700

    Initial [serious] commit

--- README.md
+++ README.md
@@ -1,7 +1,52 @@
 ### DarkenedAges &mdash; a twister-based game of intrigue and bad crypto
 
+![Trust ██ like █████. Be ███████.](trust-shield.png)
+
+#### In AD 2101, peace was beginning.
+
+The Unigalitarian Church has abolished the internet. A source of a century of war, misfurtone, and mistrust.
+Instead, people are only allowed to communicate via a church sanctioned telegram system.
+
+How can they enforce this?
+How can the church be sure people aren't communicating via other means? For example a face to face talk between husband and wife, mother and daughter?
+They simply ask nicely.
+
+Every citizen has to go to a daily confession where he/she is interviewed by a priest/priestes who are well equiped and well trained to detect lies and deviant behavior.
+It's a bit like the [Voight Kampff empathy test](https://youtu.be/Umc9ezAyJv0) ;) 
+
+It's not that you are not allowed to communicate by means other than official telegrams. It's just that you have to report all such communications.
+Other parties to such a conversation also have such duties, and are also being scrutinized daily under lie-detection gear and practices.
+You simply assume they know it all already, and try to be as percise as possible, because a contradiction might start a pretty nasty investigation and waste inquisitive resources.
+
+#### Privacy (to a reasonable extent)
+The church understands the value of privacy: there's no merit for the soul in coerced righteousness. If sin can't tempt you, how can you be a saint?
+This is why no one can read your telegrams, unless you're a suspect. If the court so orders, a telegram of yours might have to be exposed.
+In order to do that, 8 keys should combined in order to expose the message (although the game will probably start with 4 or even 3 keys until we have enough players).
+Each message is encrypted with 8 different keys. These keys are distributed to 8 random citizens called trustees. You too can become a trustee to one or more telegrams.
+If you're not rich or educated enough (or just lazy), the church can manage a key crypt in your behalf.
+This way, checks and balances are kept, and those who have nothing to hide have nothing to fear.
+
+In the beginning, most people chose the easy options, and only deranged otaku teenagers bothered to manage one (It was easier for them. The Japanese manuals are the best. The English translation deliberately sucks).
+
+Then came the attack on the El-Hamdan (EH) encryption [unofficially attributed to the late Frau Tse Tung, but you didn't hear it from me ;) ][1]
+
+In AD 2102, peace was beginning to make sense.
+
+As you already know, copies of all telegrams are kept on record at the Publicly Available International Archive (PAIA) [aka Leakvile :)]
+Now that EH encryption has been broken, 
+It has created a booming black market. The currency is telegram keys (aka unredactions).
+Today, anyone who wants to make some pocket money (and who doesn't?) manages her own key crypt. They're no longer the bulky church-issued software. Systems today convert back and forth between El-Hamdan ciphers and keys and their more juicy conterparts: leakables and unredactions. The black market system where the more keys you own, the more of the actual telegram (or leakable) is exposed to you.
+You know the metadata of your adversaries. You know who they have been talking to. You might even be lucky enough to be a trustee to some of their communications.
+
+Perhaps you could trade this information with their adversaries? The possibilities are endless.
+
+### How to play
+
 More info soon, meanwhile here's how to become a player:
 
 * Every player should have a [twister](http://twister.net.co) account
 * Fork this, add a line about yourself to `players.csv`, and mention `@darkenedages` on twister with a link to your forked gist. 
 * It is recommended to follow `@darkenedages` and have `#DarkenedAges` in your profile, but the formal definition of "player" is "one who appears at `players.csv` ;)
+
+Essentially [index.html](http://bl.ocks.org/thedod/raw/7a4a81224b5bed676b00/) was produced with `python darkened.py > index.html`.
+The idea of this "full retard crypto suite" is to create an environment where everyone can have a go at code breaking.
---
+++ darkened.py
@@ -0,0 +1,158 @@
+import random
+import codecs
+import json
+
+CHAR_REDACTED = '\u2588'  # Full block.
+CHAR_CONFLICT = '\u2573'  # Box drawings light diagonal cross.
+
+
+## [['an', 'msg', 'is', 'a', 'list', 'of', 'lines,'],
+##  ['where', 'each', 'line', 'is', 'a', 'list', 'of', 'words.']]
+def str2msg(s):
+    "explode a string into an msg"
+    return list(map(lambda l:l.split(), s.splitlines()))
+
+def msg2str(ws):
+    "implode an msg into a string"
+    return '\n'.join(map(lambda l:' '.join(l), ws))
+
+def mapmsgs(func, *msgs):
+    """map func word-per-word on all corresponding words inside [a sequence of] msgs of the same structure. E.g.
+>>> mapmsgs(lambda word1,word2:'({}/{})'.format(word1,word2),str2msg("a b\nc d"),str2msg("x y\nz w"))
+[['(a/x)', '(b/y)'], ['(c/z)', '(d/w)']]"""
+    return list(map(lambda s:list(map(lambda v:func(*v), zip(*s))), zip(*msgs)))
+
+def msgget(msg,key,default=None):
+    return mapmsgs(lambda d:d.get(key,default),msg)
+
+## conversion helpers
+def str2bytes(s):
+    return bytes(s,'utf8')
+
+def bytes2str(b):
+    return str(b,'utf8','replace')
+
+def bytes2base64(b):
+    return str(codecs.encode(b,'base64').strip(),'ascii')
+
+def base642bytes(b64):
+    if b64 is None: return None  # Might be missing
+    return codecs.decode(bytes(b64,'ascii'),'base64')
+
+## "crypto" functions
+def makepad(b):
+    """Returns a random pad with the same length as b,
+encoded to base64.
+You think this random function is weak? *Celebrate* that ;)"""
+    return random._urandom(len(b))
+
+def xor2(c1,c2):
+    return c1^c2
+
+def integrate2(a,b): 
+    if a is None or a==b: return b
+    if b is None: return a
+    return ""  # Invoke a conflict [0 is always wrong length ;)]
+
+def integrate(*args):
+    if not args: return None
+    if len(args)==1: return args[0]
+    return integrate2(args[0],integrate(*args[1:]))
+    
+    return reduce(integrate2,args)
+    
+def _redact(s):
+    plaintext = str2bytes(s)
+    pad = makepad(plaintext)
+    return {"cipher":bytes2base64(bytes(map(xor2,plaintext,pad))), "pad":bytes2base64(pad)}
+
+def _unredact(cipher,pad):
+    if pad is None:  # pad is missing
+        return(CHAR_REDACTED*len(cipher))
+    if len(cipher)!=len(pad):  # Failed the only integrity test ;)
+        return(CHAR_CONFLICT*len(cipher))
+    return str(bytes(map(xor2,cipher,pad)),'utf8','replace')
+
+def _disintegrate(value,k=8):
+    lucky = random.randint(0,k-1) 
+    return [i==lucky and value or None for i in range(k)]
+
+def disintegrate(msg,k=8):
+    zipped = mapmsgs(lambda p:_disintegrate(p,k),msg)
+    return [mapmsgs(lambda v:v[i],zipped) for i in range(k)]
+
+def redact(s,k=8):
+    redaction = mapmsgs(_redact,str2msg(s))
+    return {
+        'cipher': msgget(redaction,'cipher'),
+        'pads': disintegrate(msgget(redaction,'pad'),k)}
+
+def unredact(cipher64,*pads64):
+    return msg2str(mapmsgs(
+        _unredact,
+            mapmsgs(base642bytes,cipher64),
+            mapmsgs(base642bytes,
+                mapmsgs(integrate,*pads64))))
+
+def testit():
+    "Todo: turn this into proper unit tests, anyone?"
+    plaintext = """Here's the first line,
+followed by a second one"""
+    print('<!DOCTYPE html><html lang="en"><head><title>Testing DarkenedAges library</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head><body><pre>')
+    print('### Redacting prefab plaintext')
+    redaction = redact(plaintext,2)
+    for pad in redaction['pads']:
+        print('# pOTP >>>')
+        print(pad)
+        print('# >>> Unredaction')
+        print(unredact(redaction['cipher'],pad))
+    print('### >>> integration >>>')
+    print(unredact(redaction['cipher'],*redaction['pads']))
+    print('### Simulating incoming [prefab] ciphertext and conflicting pads')
+    cipher1 = [
+        ['lgPNUJU8', 'qLvBBQ==', '5GfdMat4', 'z8hPe4BlxxdUMXzi4w==', '2IheMA==',
+         'uZyB', 'wVCtnQ==', 'VnU=', '/pTNipiB55M=', 'XcT1gQ==', 'gxU8', '9MYX',
+         'io0='],
+        ['0fOyC2/y', 'bg8=', 'bu2T', 'rJH6dWUzpg==', 'B4BTkvtl', '+Ug=', '6wky',
+         'H78=', 'nx8=', 'TO3/fbE=', 'tqw=', 'S4k8', '+mXOsQ==', 'E3rblIM=',
+         'bIu6qbrh1g=='],
+        ['sfjl', 'JBxnLbfe0k0n', 'BShgq+QrmdquMA==', 'VYD1hx8=', 'MLk=', 'ig==',
+         'oasqOoWF', 'Asw=', 'Rof1s/gpQQONcnk=', 'ZW3zOSnTvyg=', 'QlA=',
+         '6Q0qOw=='],
+        ['9fPa', '2mFZdDltLL4=', '13vdBg==', '+eWH', 'koB24LLGnxs=', 'CedkJ3k=',
+         'b41+QUU=', 'fRkV', '4/Yc', 'anY6cA==', 'muk=', '9LrH', '5t1uXU0=',
+         'M1eqTaWs'],
+        ['8Ah2Pq4=', 'ouQ=', 'miJQVgwle6yOcg==', 'PPc=', '6jes', 'kYsY2ynZdg==']]
+    pad1 = [
+        ['xWusIv5F', '29q4dg==', 'zAa7Rc4K', 'pqY7HvIXqHA1RRWMhA==', None, None,
+         'lSXD+g==', None, None, None, None, None, None],
+        ['sJDRbhyB', None, None, None, None, 'uDs=', 'jWhA', None, '6Ho=',
+         'J4OQCp0=', '39g=', 'JuhF', 'kgS41A==', 'fR+t8fE=', None],
+        ['5ZCA', None, None, None, 'Wco=', None, None, None, 'L+mR1ohML2foHA0=',
+         None, None, None],
+        ['sbuL', 'qhM8EFAOWM0=', 'oxO8cg==', None, '/eYQidGv/nc=', 'epMLVQA=',
+         None, None, 'i5dv', 'Dh9fFA==', None, 'gNKi', 'lrEPMyg=', None],
+        [None, None, '+UoxOmBAFcvrFg==', 'Xo4=', None, '4f56t0C6WA==']]
+    pad2 = [
+        [None, None, None, None, 'nvo/RQ==', '7ebk', None, 'Pxs=',
+         'jvG/+ffvzr8=', 'KayU9Q==', '8H1Z', 'nKdz', '5OI='],
+        [None, 'GmA=', 'GoX2', '/OSQHAtXxw==', 'd+Ej94lL', None, None, 'fsw=',
+         None, None, None, None, None, None, 'AO7bwt+F+A=='],
+        [None, 'YXBKZdaztixJ', 'YEYD2Z1b7bPBXg==', 'NvKU5HQ=', None, '6w==',
+         '085ZT+nx', 'bao=', None, 'FwiAXEih3EA=', 'ICk=', 'r1l+FQ=='],
+        [None, None, None, 'jY3i', None, None, 'R/kWIDE=', 'O01B', None, None,
+         '84c=', None, 'kq8PNCM=', 'UCXLPs2F'],
+        ['h2cYGdo=', 'wIE=', None, None, 'nl/J', None]]
+    for pad in [pad1,pad2]:
+        print('# pOTP >>>')
+        #print(json.dumps(pad,indent=4))
+        print(pad)
+        print('# >>> Unredaction')
+        print(unredact(cipher1,pad))
+    print('### >>> integration >>>')
+    print(unredact(cipher1,pad1,pad2))
+
+    print('</pre></body></html>')
+
+if __name__=='__main__':
+    testit()
